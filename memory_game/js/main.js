a5de0ab1903be03ebf38c1ca15298b6ea6afbf45
Alan Bibang <alanbibang@Alans-Air.home>
2019-02-18 21:08:11 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,10 @@
 console.log("Up and running!");
 
+
 var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
 var cardFour = "queen";
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardTwo);
