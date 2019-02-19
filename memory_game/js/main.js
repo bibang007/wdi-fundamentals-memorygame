3637eeaca009a3e065fc3fc218ab6ca0b3da963a
Alan Bibang <alanbibang@Alans-Air.home>
2019-02-18 21:46:25 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,30 @@
 console.log("Up and running!");
 
 
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+
+var cardOne = cards[0];
+cardsInPlay.push(cards[0]);
+console.log(cardsInPlay);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cards[2]);
+
+if (cardsInPlay.length === 2){
+	result = true;
+} else if (cardsInPlay[0] === cardsInPlay[1]){
+
+    alert("You found a match!");
+} else {
+
+	alert("Sorry,try again.");
+
+};
+
+
+
+
